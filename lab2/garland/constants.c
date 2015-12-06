ebcffef11377d8f348afd4b36f4ad788b95c588f
Aleksey Kholovchuk <vortexxx192@gmail.com>
2015-12-07 00:51:33 +0200

    Basic setup for garland subtask

---
+++ lab2/garland/constants.c
@@ -0,0 +1,5 @@
+#include "constants.h"
+
+
+const key_t SHM_KEY = 0xBEC;
+const size_t SHM_SIZE = 0xDEAD;
---
+++ lab2/garland/constants.h
@@ -0,0 +1,7 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+
+extern const key_t SHM_KEY;
+extern const size_t SHM_SIZE;
